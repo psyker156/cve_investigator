44e17d4548eaa71ff15e7c0dfe6c18ab4b9cbb6e
Philippe Godbout <242220+psyker156@users.noreply.github.com>
2026-05-10 20:43:03 -0400

    Dependencies update
    Small dependency update and version bump

--- Tools/configuration.py
+++ Tools/configuration.py
@@ -14,11 +14,11 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-CVE_INVESTIGATOR_VERSION = '1'
-CVE_INVESTIGATOR_RELEASE_DATE = '2025-02-16'
+CVE_INVESTIGATOR_VERSION = '1.1'
+CVE_INVESTIGATOR_RELEASE_DATE = '2026-05-10'
 CVE_INVESTIGATOR_WEBSITE_URL = 'https://www.vulnarium.com'
 CVE_INVESTIGATOR_SOURCE_URL = 'https://github.com/psyker156/cve_investigator'
-CVE_INVESTIGATOR_COPYRIGHT = ('\nCopyright (C) 2025 Philippe Godbout\n'
+CVE_INVESTIGATOR_COPYRIGHT = ('\nCopyright (C) 2026 Philippe Godbout\n'
                               'This program is free software: you can redistribute it and/or modify\n'
                               'it under the terms of the GNU General Public License as published by\n'
                               'the Free Software Foundation, either version 3 of the License, or\n'
